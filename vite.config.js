9a1747ada3ecd659faff1f26bf8ca6448e4e17fb
itzgoldenheart777 <163869085+itzgoldenheart777@users.noreply.github.com>
2026-04-01 09:18:44 +0530

    chore: updated base path for new repo

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Rajput-bills/',
+  base: '/Rajput-Bills-v2/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
